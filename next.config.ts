3a7b01dcb89a8d5d60a22b6fcda21b29ee3af7fd
mdawlad29 <mdawladbdinfo@gmail.com>
2025-02-10 22:11:12 +0600

    Next.config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,72 @@
-import type { NextConfig } from "next";
+// @ts-check
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "*",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.google.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "dev-agent.localbet.xyz",
+        pathname: "**",
+      },
+    ],
+
+    formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG: true,
+    unoptimized: false,
+    loader: "custom",
+    // path: "/",
+    loaderFile: "./loader.ts",
+  },
+  output: "standalone",
+  distDir: ".next",
 };
 
 export default nextConfig;
